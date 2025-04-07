9f761da1ff7e29c7e2a7d5375a5301528a746a1c
jainAmishhh <jainamish1511@gmail.com>
2025-04-07 22:21:05 +0530

    Add logging functionality with Winston and Morgan, and create app.log for request logging

--- index.js
+++ index.js
@@ -1,72 +1,92 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middleware
-app.use(express.json())
+app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let teaId = 1;
 
 // CRUD operations
 app.post("/teas", (req, res) => {
-    const { name, price } = req.body;
-    const newTea = {
-        id: teaId++,
-        name,
-        price
-    };
-    teaData.push(newTea);
+  const { name, price } = req.body;
+  const newTea = {
+    id: teaId++,
+    name,
+    price,
+  };
+  teaData.push(newTea);
 
-    res.status(201).send(newTea);
-})
+  res.status(201).send(newTea);
+});
 
 app.get("/teas", (req, res) => {
-    res.status(200).send(teaData);
-})
+  res.status(200).send(teaData);
+});
 
 app.get("/teas/:id", (req, res) => {
-    const tea = teaData.find(t => t.id === parseInt(req.params.id));
-    
-    if(!tea) {
-        return res.status(404).send("Tea not found");
-    }
-    res.status(200).send(tea);
-})
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  res.status(200).send(tea);
+});
 
 app.put("/teas/:id", (req, res) => {
-    const tea = teaData.find(t => t.id === parseInt(req.params.id));
-    
-    if(!tea) {
-        return res.status(404).send("Tea not found");
-    }
-    
-    const { name, price } = req.body;
-    tea.name = name;
-    tea.price = price;
-    res.status(200).send(tea)
-})
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.status(200).send(tea);
+});
 
 app.delete("/teas/:id", (req, res) => {
-    const index = teaData.find(t => t.id === parseInt(req.params.id));
-    
-    if(index === -1) {
-        return res.status(404).send("Tea not found");
-    }
-    teaData.splice(index, 1);
-    return res.status(204).send("Tea deleted!!!");
-})
+  const index = teaData.find((t) => t.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    return res.status(404).send("Tea not found");
+  }
+  teaData.splice(index, 1);
+  return res.status(204).send("Tea deleted!!!");
+});
 
 app.get("/", (req, res) => {
-    res.send("Heyy you all!!!");
-})
+  res.send("Heyy you all!!!");
+});
 
 app.get("/konsi-tea-chahiye", (req, res) => {
-    res.send("What tea would you prefer?");
-})
+  res.send("What tea would you prefer?");
+});
 
 app.listen(port, () => {
-    console.log(`Server is runnig at the port: ${port}....`);
+  console.log(`Server is runnig at the port: ${port}....`);
 });
